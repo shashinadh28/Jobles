48b75305f0c2670f67ef1013b54502f706ab50be
Durgashashinad <durgashashinadwork2@gmail.com>
2025-04-12 19:06:10 +0530

    Fixed Google Analytics implementation

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -6,24 +6,20 @@ export default function GoogleAnalytics() {
   return (
     <>
       <Script
-        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=G-1MDFS8KLQP`}
-      />
-      <Script
-        id="google-analytics"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-1MDFS8KLQP', {
-              page_path: window.location.pathname,
-              stream_id: '10478746494'
-            });
-          `,
-        }}
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1MDFS8KLQP', {
+            page_path: window.location.pathname,
+            stream_id: '10478746494'
+          });
+        `}
+      </Script>
     </>
   );
 } 
